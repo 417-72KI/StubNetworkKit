3f5816853a153e9dce74fed37d25b9368ec947c8
417-72KI <417.72ki@gmail.com>
2022-04-28 03:56:12 +0900

    create stub setup functions

--- Sources/StubNetworkingSwift/StubNetworkingSwift.swift
+++ Sources/StubNetworkingSwift/StubNetworkingSwift.swift
@@ -1,6 +1,25 @@
-public struct StubNetworkingSwift {
-    public private(set) var text = "Hello, World!"
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
-    public init() {
-    }
+// MARK: - Setup stub
+
+public var defaultStubSession: URLSession {
+    let configuration = URLSessionConfiguration.ephemeral
+    registerStub(to: configuration)
+    return URLSession(configuration: configuration)
+}
+
+public func registerStub(to configuration: URLSessionConfiguration) {
+    configuration.protocolClasses = [StubURLProtocol.self]
+}
+
+/// Handle all requests via `URLSession.shared`
+public func registerStubForSharedSession() {
+    precondition(URLProtocol.registerClass(StubURLProtocol.self))
+}
+
+public func unregisterStubForSharedSession() {
+    URLProtocol.unregisterClass(StubURLProtocol.self)
 }
