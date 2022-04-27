1312d9ee31b3d88a9e68274baf8f5b5c2ff74e42
417-72KI <417.72ki@gmail.com>
2022-04-28 04:09:29 +0900

    test for matchers with Scheme

--- Tests/StubNetworkingSwiftTests/StubConditionTests.swift
+++ Tests/StubNetworkingSwiftTests/StubConditionTests.swift
@@ -20,4 +20,20 @@ final class StubConditionTests: XCTestCase {
             }
         )
     }
+
+    func testScheme() throws {
+        func actual(_ url: String) -> Bool {
+            let matcher = Scheme.is("foo")
+            return matcher(URLRequest(url: URL(string: url)!))
+        }
+
+        runAll(
+            expect(actual("foo:"), is: true),
+            expect(actual("foo://"), is: true),
+            expect(actual("foo://bar/baz"), is: true),
+            expect(actual("bar://"), is: false),
+            expect(actual("bar://foo/"), is: false),
+            expect(actual("foobar://"), is: false)
+        )
+    }
 }
