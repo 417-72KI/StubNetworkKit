750fdac1b669a818548aa500e3f248c66f898a9e
417-72KI <417.72ki@gmail.com>
2022-05-01 23:44:44 +0900

    add test case for `stub()`

---
+++ Tests/StubNetworkingSwiftTests/StubTests.swift
@@ -0,0 +1,25 @@
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+import XCTest
+import StubNetworkingSwift
+import SwiftParamTest
+
+final class StubTests: XCTestCase {
+    func testStub() throws {
+        assert(to: stub(urlString: "https://foo.bar/baz")) {
+            expect(URL(string: "https://foo.bar/baz?q=1&empty=&flag")! ==> true)
+            expect(URL(string: "https://foo.bar/baz")! ==> true)
+        }
+
+        assert(to: stub(urlString: "https://foo.bar/baz?q=1&empty=&flag")) {
+            expect(URL(string: "https://foo.bar/baz?q=1&empty=&flag")! ==> true)
+            expect(URL(string: "https://foo.bar/baz")! ==> false)
+        }
+
+        assert(to: stub(urlString: "https://foo.bar/baz", method: .post)) {
+            expect((URL(string: "https://foo.bar/baz")!, .get) ==> false)
+            expect((URL(string: "https://foo.bar/baz")!, .post) ==> true)
+        }
+    }
+}
---
+++ Tests/StubNetworkingSwiftTests/Util/SwiftParamTest+Stub.swift
@@ -0,0 +1,43 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+@testable import StubNetworkingSwift
+import SwiftParamTest
+import XCTest
+
+extension XCTestCase {
+    @discardableResult
+    func assert(to stub: Stub,
+                @ParameterBuilder1<URLRequest, Bool> builder: () -> [Row1<URLRequest, Bool>]
+    ) -> ParameterizedTestResult {
+        assert(to: stub.condition, builder: builder)
+    }
+}
+
+func expect<R: Equatable>(
+    _ row: Row1<URL, R>,
+    file: StaticString = #file,
+    line: UInt = #line
+) -> Row1<URLRequest, R> {
+    expect(URLRequest(url: row.args.head) ==> row.expect, file: file, line: line)
+}
+
+func expect<R: Equatable>(
+    _ row: Row2<URL, StubNetworkingSwift.Method, R>,
+    file: StaticString = #file,
+    line: UInt = #line
+) -> Row1<URLRequest, R> {
+    var request = URLRequest(url: row.args.head)
+    request.httpMethod = { (method) -> String in
+        switch method {
+        case .get: return "GET"
+        case .post: return "POST"
+        case .put: return "PUT"
+        case .patch: return "PATCH"
+        case .delete: return "DELETE"
+        case .head: return "HEAD"
+        }
+    }(row.args.tail.head)
+    return expect(request ==> row.expect, file: file, line: line)
+}
