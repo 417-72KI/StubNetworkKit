e5cd6d09374988b1f5f12e921e2ca61ca84d0bc0
417-72KI <417.72ki@gmail.com>
2022-05-02 00:08:56 +0900

    add test case for method-chain builders

--- Tests/StubNetworkingSwiftTests/StubTests.swift
+++ Tests/StubNetworkingSwiftTests/StubTests.swift
@@ -22,4 +22,36 @@ final class StubTests: XCTestCase {
             expect((URL(string: "https://foo.bar/baz")!, .post) ==> true)
         }
     }
+
+    func testStubWithMethodChainBuilders() throws {
+        let condition1 = stub()
+            .scheme("https")
+            .host("foo.bar")
+            .path("/baz")
+            .method(.get)
+            .queryParams(["q": "1", "empty": "", "flag": nil])
+        assert(to: condition1) {
+            expect(URL(string: "https://foo.bar/baz?q=1&empty=&flag")! ==> true)
+            expect(URL(string: "https://foo.bar/baz")! ==> false)
+        }
+
+        let condition2 = stub()
+            .scheme("https")
+            .host("foo.bar")
+            .path("/baz")
+            .method(.post)
+            .jsonBody(["q": 1, "lang": "ja", "flag": true])
+
+        var request = URLRequest(url: URL(string: "https://foo.bar/baz")!)
+        request.httpMethod = "POST"
+        request.httpBody = #"{"q": 1, "lang": "ja", "flag": true}"#.data(using: .utf8)
+
+        assert(to: condition2) {
+            expect(request ==> true)
+            expect((URL(string: "https://foo.bar/baz?q=1&empty=&flag")!, Method.post) ==> false)
+            expect(URL(string: "https://foo.bar/baz?q=1&empty=&flag")! ==> false)
+            expect(URL(string: "https://foo.bar/baz")! ==> false)
+        }
+
+    }
 }
