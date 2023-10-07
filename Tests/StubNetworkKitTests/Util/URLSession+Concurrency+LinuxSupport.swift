0ebf2669e090f37c08fde056a78319ab8eaf7a6f
417-72KI <417.72ki@gmail.com>
2023-10-07 22:42:20 +0900

    add Linux support for `data(for:delegate:)`

--- Tests/StubNetworkKitTests/Util/URLSession+Concurrency+LinuxSupport.swift
+++ Tests/StubNetworkKitTests/Util/URLSession+Concurrency+LinuxSupport.swift
@@ -5,9 +5,27 @@ import FoundationNetworking
 
 #if !(os(macOS) || os(iOS) || os(watchOS) || os(tvOS)) && compiler(>=5.6) && canImport(_Concurrency)
 extension URLSession {
-    func data(from url: URL, delegate: URLSessionTaskDelegate? = nil) async throws -> (Data, URLResponse) {
+    func data(
+        from url: URL,
+        delegate: URLSessionTaskDelegate? = nil
+    ) async throws -> (Data, URLResponse) {
         try await withCheckedThrowingContinuation { cont in
             dataTask(with: url) { data, response, error in
+                if let error = error {
+                    cont.resume(throwing: error)
+                    return
+                }
+                cont.resume(returning: (data!, response!))
+            }.resume()
+        }
+    }
+
+    func data(
+        for request: URLRequest,
+        delegate: (URLSessionTaskDelegate)? = nil
+    ) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { cont in
+            dataTask(with: request) { data, response, error in
                 if let error = error {
                     cont.resume(throwing: error)
                     return
