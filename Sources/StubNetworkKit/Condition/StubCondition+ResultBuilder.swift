78a8874af6210d0d0256c095718e43c46be6059a
417-72KI <417.72ki@gmail.com>
2024-05-03 01:10:46 +0900

    fix `ExistentialAny`

--- Sources/StubNetworkKit/Condition/StubCondition+ResultBuilder.swift
+++ Sources/StubNetworkKit/Condition/StubCondition+ResultBuilder.swift
@@ -5,37 +5,37 @@ import FoundationNetworking
 
 @resultBuilder
 public struct StubConditionBuilder {
-    public static func buildBlock(_ components: StubCondition...) -> StubCondition {
+    public static func buildBlock(_ components: any StubCondition...) -> any StubCondition {
         components.reduce(alwaysTrue) { AnyStubCondition($0) && AnyStubCondition($1) }
     }
 
-    public static func buildOptional(_ component: StubCondition?) -> StubCondition {
+    public static func buildOptional(_ component: (any StubCondition)?) -> any StubCondition {
         component ?? alwaysTrue
     }
 
-    public static func buildEither(first component: StubCondition) -> StubCondition {
+    public static func buildEither(first component: any StubCondition) -> any StubCondition {
         component
     }
 
-    public static func buildEither(second component: StubCondition) -> StubCondition {
+    public static func buildEither(second component: any StubCondition) -> any StubCondition {
         component
     }
 }
 
 // MARK: -
-public func stubCondition(@StubConditionBuilder builder: () -> StubCondition) -> StubCondition {
+public func stubCondition(@StubConditionBuilder builder: () -> any StubCondition) -> any StubCondition {
     builder()
 }
 
 @discardableResult
-public func stub(@StubConditionBuilder builder: () -> StubCondition,
+public func stub(@StubConditionBuilder builder: () -> any StubCondition,
                  withResponse stubResponse: @escaping (URLRequest) -> StubResponse) -> Stub {
     stub(stubCondition(builder: builder),
          withResponse: stubResponse)
 }
 
 @discardableResult
-public func stub(@StubConditionBuilder builder: () -> StubCondition) -> Stub {
+public func stub(@StubConditionBuilder builder: () -> any StubCondition) -> Stub {
     stub(stubCondition(builder: builder),
          withResponse: errorResponse(.unimplemented))
 }
